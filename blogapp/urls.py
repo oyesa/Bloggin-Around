2f3a9fca54042647b9331bf94798e93976d87d91
oyesa <mercyoyesa2018@gmail.com>
2022-08-15 19:33:59 +0300

    create urls file and templates folder

---
+++ blogapp/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.urls import path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+urlpatterns=[
+  
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogapp.apps.BlogappConfig',
+    'cloudinary',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
