da5b533036e1d48cc2b66d3bedae1389a852be27
oyesa <mercyoyesa2018@gmail.com>
2022-08-15 20:52:03 +0300

    update view for blogposts and home

--- blogapp/views.py
+++ blogapp/views.py
@@ -8,12 +8,27 @@ import cloudinary
 
 # Create your views here.
 def home(request):
-  return render(request, 'home.html')
+  blogposts=Blogpost.objects.all().order_by('blogpost_date') #to view all blogposts available
+  return render(request, 'home.html', {'blogposts':blogposts})
 
 
 def save_blogpost(request):
   if request.method=='POST':
     blog_title=request.POST['blog_title']
-    
+    blog_content=request.POST['blog_content']
+    blog_image=request.FILES['blog_image']
+    blog_image=cloudinary.uploader.upload(blog_image)
+
+    blogpost=Blogpost(blog_title=blog_title, blog_content=blog_content, blog_image=blog_image)
+    blogpost.save_blogpost()
+
+    return redirect('/home', {'success': 'Blogpost Sucessfully Uploaded'})
+  else:
+    return render(request, 'home.html', {'danger': 'Blogpost Not Uploaded'})
 
     
+
+def save_comment(request):
+  if request.method=='POST':
+    comment=request.POST['comment']
+        
