61b543234a6d41ccadba369a1b1df41a2f0cc057
oyesa <mercyoyesa2018@gmail.com>
2022-08-18 00:28:09 +0300

    create contact template and add navbar details

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls.static import static
 urlpatterns=[
   path('', views.home, name='home'),
   path('about', views.about, name='about'),
+  path('contact', views.contact, name='contact')
 ]
 
 if settings.DEBUG:
